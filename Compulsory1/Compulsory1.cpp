6e320f5d19945b781b0324662ac69298eca0a7ca
Mathias <Cliffback@users.noreply.github.com>
2020-09-16 18:55:27 +0200

    added more comments

--- Compulsory1/Compulsory1.cpp
+++ Compulsory1/Compulsory1.cpp
@@ -132,21 +132,21 @@ void PlayerTurn()
 // Checking if either player has won
 bool gamover()
 {
+	// Checks if won by horizontal or vertical 
 	for (int i = 0; i < 3; i++)
 	{
 		if (Board[i][0] == Board[i][1] && Board[i][0] == Board[i][2] || Board[0][i] == Board[1][i] && Board [0][i] == Board[2][i])
 		{
 			return false;
 		}
-
-
-
 	}
+	// Checks if won by getting either line in a cross
 	if (Board[0][0] == Board[1][1] && Board[0][0] == Board[2][2] || Board[0][2] == Board[1][1] && Board[0][2] == Board[2][0])
 	{
 		return false;
 	}
 
+	// Checks if all spots are chosen and returns draw if so
 	for (int i = 0; i < 3; i++)
 		for (int j = 0; j < 3; j++)
 		{
@@ -201,9 +201,6 @@ int main()
 
 
 	}
-
-
-
 
 	return 0;
 }
